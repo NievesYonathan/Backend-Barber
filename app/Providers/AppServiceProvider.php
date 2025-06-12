1d187f2cf74cc3274c4d0e0469311e4f14377a81
Yonathan Nieves <yonathannieves17@gmail.com>
2025-06-12 00:31:52 -0500

    PDO y Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,10 @@ use App\Services\Cita\CitaService;
 use App\Services\Categoria\CategoriaService;
 use App\Services\Categoria\Interfaces\CategoriaServiceInterface;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,8 +26,18 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(Request $request): void
     {
-        //
+        if ($this->app->environment('production')) {
+        $request->setTrustedProxies(
+                [$request->getClientIp()],
+                SymfonyRequest::HEADER_X_FORWARDED_FOR |
+                SymfonyRequest::HEADER_X_FORWARDED_HOST |
+                SymfonyRequest::HEADER_X_FORWARDED_PORT |
+                SymfonyRequest::HEADER_X_FORWARDED_PROTO
+            );
+
+            URL::forceScheme('https');
+        }
     }
 }
