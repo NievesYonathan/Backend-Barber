2d961e45764528c119c7f02b6fa43b9f300832c4
Yonathan Nieves <yonathannieves17@gmail.com>
2025-06-12 00:24:55 -0500

    Se agrega el cors para permitir el consumo de rutas

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
     ];
 
